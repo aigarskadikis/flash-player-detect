cee6ff450a5923487a1e4b658d600b8708d23a73
catonrug <aigars.kadikis@gmail.com>
2016-01-17 12:27:27 +0200

    bery begining
    probably not working

--- check.sh
+++ check.sh
@@ -201,61 +201,37 @@ if [ ! -f "/home/pi/client_secrets.json" ]
 fi
 
 
-#put all links that contains "BundleId" in array
-linklist=$(wget --no-cookies --no-check-certificate https://www.java.com/en/download/manual.jsp -qO- | grep BundleId | sed "s/\d034/\n/g" | grep "^http" | sort | uniq | sed '$aend of file')
+url=$(echo http://fpdownload.macromedia.com/pub/flashplayer/latest/help/install_flash_player_ax.exe)
+echo url is $url
+filename=$(echo $url | sed "s/^.*\///g")
+echo filename is $filename
+wget $url -O $tmp/$filename -q
+sha1=$(sha1sum $tmp/$filename -q | sed "s/\s.*//g")
+echo sha1 is $sha1
+grep "$sha1" $db
+if [ $? -ne 0 ]
+#if sha1 sum do not exist in database then this is new version
+then
+echo New Flash Player AX version detected
 
+#creating md5 checksumsome check sums
+md5=$(md5sum $tmp/$filename | sed "s/\s.*//g")
+echo md5 is $md5
 
-printf %s "$linklist" | while IFS= read -r url
-do {
-	#echo $url
-	
-	#take every link and look what kind of file it reports. do not download anything yet!
-	wget $url -S --spider -o $tmp/outs.log -q
-	
-	#detect if there is any exe files in the link
-	sed "s/http/\nhttp/g;s/exe/exe\n/g" $tmp/outs.log | grep "^http.*x64.exe$\|^http.*i586.exe$" | sort | uniq | grep "^http.*x64.exe$\|^http.*i586.exe$" > /dev/null
-		if [ $? -eq 0 ]
-			#if some exe file was found
-			then
-				#detect exact filename by striping down all content from left side and leaving only last part
-				filename=$(sed "s/http/\nhttp/g;s/exe/exe\n/g" $tmp/outs.log | grep "^http.*x64.exe$\|^http.*i586.exe$" | sort | uniq | sed "s/^.*\///g")
-				
-				#check if this can of filename has already been downloaded
-				cat $db | grep "$filename"
-					if [ $? -ne 0 ]
-						#if this file has never downloaded
-						then
-							echo Downloading $filename
-							#downloading real file now!
-							wget --no-cookies --no-check-certificate $url -O $tmp/$filename -q
-							#creating some check sums
-							md5=$(md5sum $tmp/$filename | sed "s/\s.*//g")
-							echo $md5
-							sha1=$(sha1sum $tmp/$filename | sed "s/\s.*//g")
-							echo $sha1
-							#lets put all signs about this file into the database
-							echo "$url">>$db
-							echo "$filename">> $db
-							echo "$md5">> $db
-							echo "$sha1">> $db
-								#if google drive config exists then upload and delete file:
-								if [ -f "../gd/$appname.cfg" ]
-									then
-										echo Uploading $filename to Google Drive..
-										../uploader.py "../gd/$appname.cfg" "$tmp/$filename"
-								fi
-							#lets send emails to all people in "posting" file
-							emails=$(cat ../posting | sed '$aend of file')
-							printf %s "$emails" | while IFS= read -r onemail
-							do {
-								python ../send-email.py "$onemail" "$filename" "$url
-$md5
-$sha1"
-							} done
-							echo
-					fi
-		fi
-} done
+#lets put all signs about this file into the database
+echo "$md5">> $db
+echo "$sha1">> $db
+			
+#check what exact version this is
+7z x $tmp/$filename -y -o$tmp
+versionax=$(cat $tmp/.rsrc/MANIFEST/1 | \
+sed "s/<dependency>/\n<dependency>\n/g" | \
+sed "s/<\/assembly>/\n<\/assembly>\n/g" | \
+sed "/<dependency>/,/<\/assembly>/d" | \
+sed "s/\d034/\n/g" | \
+grep "^[0-9]*.\.[0-9]*.\.[0-9]*.\.[0-9]")
+echo $versionax
+fi
 
 #clean and remove whole temp direcotry
 rm $tmp -rf > /dev/null
